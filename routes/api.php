d37c1b43d50dcbc4a75f51ba676e672f638a1858
dijahmac <mcdonald.khadijah@gmail.com>
2017-08-28 15:36:18 -0400

    added the stuff

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('signup','AuthController@signUp');
+Route::post('signIn', 'AuthController@signIn');
+Route::get('getUser', 'AuthController@getUser');
 
 Route::any('{path?}', 'MainController@index')->where("path", ".+");
